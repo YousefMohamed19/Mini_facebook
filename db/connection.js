dd1be8d1896515274751166862c00f387ec1231a
YousefMohamed19 <yousef01121977646@gmail.com>
2024-06-08 02:54:55 +0300

    database

--- db/connection.js
+++ db/connection.js
@@ -1,8 +1,5 @@
 import { Sequelize } from 'sequelize';
-export const sequelize = new Sequelize('mini_facebook', 'root', '', {
-    host: 'localhost', 
-    dialect: 'mysql'
-});
+export const sequelize = new Sequelize('mysql://utxhxqh5i8euypzy:MpmIFKWZD02M5A6rQwYm@bmiuheoqo0rvs2qglxsy-mysql.services.clever-cloud.com:3306/bmiuheoqo0rvs2qglxsy');
 
 export const connectDB = () => {sequelize.authenticate().then(() => {
     console.log('Connection has been established successfully.');
